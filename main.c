f6b4196aaf90fbf52cd637002e329dd0b5a96454
SinX <punisherdu28@gmail.com>
2023-05-22 14:28:20 +0200

    var

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
-//Header files
+//Header files login token to git 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
 #include <stdio.h>
+#include <string.h>
+
+char* nom="Gold D. Roger";
 
 int main(){
-
-    printf("Hello Roger! \n");
+    
+    printf("Hello %s!\n",nom);
+    
     return 0;
 
 }
